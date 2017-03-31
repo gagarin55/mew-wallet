798f631a2c27d59cdfb57b997e2e9487b52d508c
kvhnuke <kvhnuke@aol.com>
2017-03-31 02:51:29 -0700

    monero wallet generation

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict'
 var HDWallet = require('./src/HDWallet')
+    /*var cnUtil = require('./lib/monero/cnUtil')
+    var crypto = require('crypto')
+    var cn = new cnUtil();
+    var seed = crypto.randomBytes(32).toString('hex');
+    console.log(seed)
+    console.log(cn.create_address(seed));*/
 module.exports = {
     HDWallet: HDWallet
 }
